7fe3f772969a22397faff1707cf6cf953e9ae604
Vyacheslav Byrdaev <smit467bs@gmail.com>
2023-05-06 20:34:10 +0300

    feat: task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const src = path.join(__dirname, 'files');
+const srcCopy = path.join(__dirname, 'files-copy');
+
+function createFolder() {
+	fs.mkdir(srcCopy, {recursive: true}, err => {
+		if (err) throw err;
+
+		console.log('Папка создана!');
+	})
+}
+
+function copyFiles() {
+	fs.readdir(src,
+		{withFileTypes: true},
+		(err, files) => {
+			if (err) throw err;
+			files.forEach(file => {
+				fs.copyFile(path.join(src, `${file.name}`),
+					path.join(srcCopy, `${file.name}`),
+					err => {
+						if (err) throw err;
+						console.log(`${file.name} Скопирован`);
+					});
+			})
+		})
+
+}
+
+
+fs.access(srcCopy, err => {
+	if (err) {
+		createFolder()
+		copyFiles();
+
+	} else {
+		fs.rm(srcCopy, {recursive: true}, err => {
+			if (err) throw err;
+			createFolder()
+			copyFiles();
+		})
+	}
+})
+
 
